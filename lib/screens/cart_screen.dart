46eed149fed6fb460c14b5ca1eec09905f360f2d
offlineProgrammer <mmalaka@gmail.com>
2021-02-06 14:53:00 -0800

    CartItem

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_shop_app/providers/cart.dart';
+import 'package:my_shop_app/providers/cart.dart' show Cart;
+import 'package:my_shop_app/widgets/cart_item.dart';
 import 'package:provider/provider.dart';
 
 class CartScreen extends StatelessWidget {
@@ -46,6 +47,19 @@ class CartScreen extends StatelessWidget {
               ),
             ),
           ),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (ctx, i) => CartItem(
+                  id: cart.items.values.toList()[i].id,
+                  price: cart.items.values.toList()[i].price,
+                  quantity: cart.items.values.toList()[i].quantity,
+                  title: cart.items.values.toList()[i].title),
+              itemCount: cart.cartCount,
+            ),
+          )
         ],
       ),
     );
